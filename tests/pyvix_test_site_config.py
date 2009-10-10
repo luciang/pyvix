73dd44f331e8411152efd939c0a2107fe782725e
Lucian Adrian Grijincu <lucian.grijincu@gmail.com>
2009-10-11 00:58:01 +0300

    add a default pyvix_test_site_config file
    Signed-off-by: Lucian Adrian Grijincu <lucian.grijincu@gmail.com>

---
+++ tests/pyvix_test_site_config.py
@@ -0,0 +1,9 @@
+
+
+# path to a .vmx that will be used in testing
+# this vmx will be started/stopped/suspended
+# it's snapshots will be deleted and/or created
+
+generic_vmx = '/home/so/vmware/so-vm-debian-sid/faust.vmx'
+
+
